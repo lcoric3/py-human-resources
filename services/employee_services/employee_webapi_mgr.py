ba5d126101e10aa22c24fcdabd117f8e0c149c6e
lcoric3 <luka.coric3@gmail.com>
2024-02-15 19:15:34 +0100

    Summary

--- services/employee_services/employee_webapi_mgr.py
+++ services/employee_services/employee_webapi_mgr.py
@@ -1,3 +1,10 @@
 # Sve funkcije vezane uz manipulaciju podacima djelatnika 
 # na REST api serveru
 import requests
+
+
+# TODO implement try except
+response = requests.get('https://jsonplaceholder.typicode.com/users/1')
+if response.status_code == 200:
+    print(response.json())
+
